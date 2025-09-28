5cb30ff55f88135822bb2e16a793f6a4f8ae6eae
Joeyvella <joey.vella@my.jcu.edu.au>
2025-09-28 11:09:12 +1000

    definitions of get_valid_score and determine_score added.

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -23,3 +23,23 @@ def main():
         choice = input(">>> ").upper()
 
     print("Farewell.")
+
+def get_valid_score():
+        score = float(input("Enter score (0-100): "))
+        while score < 0 or score > 100:
+            print("Invalid score! Must be between 0 and 100.")
+            score = float(input("Enter score (0-100): "))
+        return score
+
+def determine_score(score):
+    if score < 0 or score > 100:
+        return "Invalid"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Pass"
+    else:
+        return "Bad"
+
+
+main()
