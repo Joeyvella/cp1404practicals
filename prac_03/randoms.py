fb441c90695f5f3ba30ce0a8aa01d7aba41b7907
Joeyvella <joey.vella@my.jcu.edu.au>
2025-10-02 13:32:38 +1000

    Results for line 1 and 2 after entering the random integer codes in python console

---
+++ prac_03/randoms.py
@@ -0,0 +1,5 @@
+# results for line 1,( 17, 13,12, 20)
+# Line 1. the smallest number i could have seen was a 5 with the max being a 20, my results ranged from 12 to 20
+
+#Results for line 2,(9, 9, 5, 9)
+#Line 2.
