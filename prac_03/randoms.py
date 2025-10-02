cfd43aa9421a9cd8d6e0d24780a5a1a957b9be81
Joeyvella <joey.vella@my.jcu.edu.au>
2025-10-02 13:55:54 +1000

    results for line 3 and code to gain a random number between 1 and 100 inclusive

--- prac_03/randoms.py
+++ prac_03/randoms.py
@@ -2,4 +2,11 @@
 # Line 1. the smallest number i could have seen was a 5 with the max being a 20, my results ranged from 12 to 20
 
 #Results for line 2,(9, 9, 5, 9)
-#Line 2.
+#Line 2. the smallest number I can get is 3 with a max of 10 with a stepping of 2, the max number with the steps I could have gotten was a 9
+# I would not be able to get a 4 as there is a step of 2, this means from 3 it would step to a 5
+
+#Results for line 3,(2.555930763954662, 4.692903441503153, 3.999844785777401, 2.8269971394339515)
+# the smallest number would be 2.5 with the largest being 5.5
+
+import random
+print(random.randint(1,100))
