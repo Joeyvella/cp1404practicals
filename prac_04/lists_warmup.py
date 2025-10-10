86d9cf16c8a57ef80dfdafb3ffae04e1783e2fdd
Joeyvella <joey.vella@my.jcu.edu.au>
2025-10-10 19:14:27 +1000

    Answer questions for the warmup

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,10 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+#numbers[0]- 3
+#numbers[-1]- 2
+#numbers[3]- 1
+#numbers[:-1]- reverse
+#numbers[3:4] - 1
+#5 in numbers -  True
+#7 in numbers - False
+#"3" in numbers - False as it is asking for a string
+#numbers + [6, 5, 3] - [3,1,4,1,5,9,2,6,5,3] (adds 6 5 3 to the list)
