677e9704df9ac2754503815e85b755ab88a332c0
Joeyvella <joey.vella@my.jcu.edu.au>
2025-10-11 10:47:05 +1000

    Function that loads subjects strips then parts them to read it

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -5,44 +5,27 @@ Data file -> lists program
 
 FILENAME = "subject_data.txt"
 
-
 def main():
-    """Loads in subject records and prints a formatted report"""
-    subjects = load_data(FILENAME)
+    """Reads the subjects data"""
+    subjects = load_subjects(FILENAME)
     display_subjects(subjects)
 
-
-def load_data(filename=FILENAME):
-    """Read data from file formatted like: subject,lecturer,number of students."""
-
+def load_subjects(filename=FILENAME):
+    """loads that data from txt file"""
     subjects = []
-    with open(filename) as input_file
-        for line in input_file:
-            line = line.strip()
-            subject_code, lecturer, count_str = line.split(",")
-            student_count = int(count_str)
-            subjects.append([subject_code, lecturer, student_count])
+    input_file = open(filename)
+    for line in input_file:
+        line = line.strip()
+        parts = line.split(',')
+        parts[2] = int(parts[2])
+        subjects.append(parts)
+    input_file.close()
     return subjects
 
 
-def display_subjects(subjects):
-    """Prints line that describes the records"""
-    for subject_code, lecturer, student_count in subjects:
-        print(f"{subject_code} is taught by {lecturer} and has {student_count} students")
 
 
-
-    input_file = open(filename)
-    for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
-        line = line.strip()  # Remove the \n
-        parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
-        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
-    input_file.close()
 
 
 main()
+
