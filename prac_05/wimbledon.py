7fa53305c31583d796b5dfc9ae44798059ca7220
Joeyvella <joey.vella@my.jcu.edu.au>
2025-10-19 20:13:26 +1000

    Read data and count it to find needed information.

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -15,3 +15,22 @@ def main():
 
     display_champions(champion_to_count)
     display_countries(countries)
+
+def get_wimbledon_data(filename):
+    """Read the data file and return a list of lists."""
+    with open(filename, "r", encoding="utf-8-sig") as in_file:
+        in_file.readline()  # Skip the header line
+        data = [line.strip().split(",") for line in in_file]
+    return data
+
+
+def count_champions(data):
+    """Count how many times each champion has won."""
+    champion_to_count = {}
+    for record in data:
+        champion = record[2]
+        if champion in champion_to_count:
+            champion_to_count[champion] += 1
+        else:
+            champion_to_count[champion] = 1
+    return champion_to_count
