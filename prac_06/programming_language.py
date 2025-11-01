72c99f4b1c4510ba6fcd15d45e16ffcbffe7902d
Joeyvella <joey.vella@my.jcu.edu.au>
2025-11-01 15:50:59 +1000

    Import the class file over to this file, the main function will also print what languages are dynamic

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -9,3 +9,11 @@ class ProgrammingLanguage:
         self.typing = typing
         self.reflection = reflection
         self.year = year
+
+    def is_dynamic(self):
+        """Return True if language is dynamic"""
+        return self.typing.lower()== "dynamic"
+
+    def __str__(self):
+        """Neatly format a string for the language"""
+        return f"{self.name}"
