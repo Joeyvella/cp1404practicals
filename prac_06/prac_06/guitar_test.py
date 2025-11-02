401963770149cfb1944004a77ae35af617b7bb20
Joeyvella <joey.vella@my.jcu.edu.au>
2025-11-02 13:53:50 +1000

    Test the get_age() and is_vintage

--- prac_06/prac_06/guitar_test.py
+++ prac_06/prac_06/guitar_test.py
@@ -6,3 +6,9 @@ def main():
 
     gibson = Guitar("Gibson L-5 CES", 1922, 16035.40)
     another = Guitar("Another Guitar", 2013, 500)
+
+    print("Gibson L-5 CES get_age() - Expected 100. Got", gibson.get_age())
+    print("Another Guitar get_age() - Expected 9. Got", another.get_age())
+
+    print("Gibson L-5 CES is_vintage() - Expected True. Got", gibson.is_vintage())
+    print("Another Guitar is_vintage() - Expected False. Got", another.is_vintage())
