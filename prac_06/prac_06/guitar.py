13546fc4baf58f11cbf4c7264e4add534c20ec40
Joeyvella <joey.vella@my.jcu.edu.au>
2025-11-02 13:36:08 +1000

    Use init to construct string

---
+++ prac_06/prac_06/guitar.py
@@ -0,0 +1,14 @@
+"""
+CP1404/CP5632 Practical
+Guitar class
+Estimate: 20
+Actual:
+"""
+class Guitar:
+    """Represent a Guitar object."""
+
+    def __init__(self, name="", year=0, cost=0):
+        """Initialise a Guitar instance."""
+        self.name = name
+        self.year = year
+        self.cost = cost
