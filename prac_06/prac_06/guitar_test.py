b9cb754be173be5636313a758769864a45e78f38
Joeyvella <joey.vella@my.jcu.edu.au>
2025-11-02 13:52:44 +1000

    set up test data

---
+++ prac_06/prac_06/guitar_test.py
@@ -0,0 +1,8 @@
+from guitar import Guitar
+
+
+def main():
+    """Test the Guitar class methods."""
+
+    gibson = Guitar("Gibson L-5 CES", 1922, 16035.40)
+    another = Guitar("Another Guitar", 2013, 500)
