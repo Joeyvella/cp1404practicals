8c373cb6f18f56c0761f5fe79397d2c18ffaba10
Joeyvella <joey.vella@my.jcu.edu.au>
2025-11-02 13:40:06 +1000

    Print using str to make it print nice

--- prac_06/prac_06/guitar.py
+++ prac_06/prac_06/guitar.py
@@ -12,3 +12,8 @@ class Guitar:
         self.name = name
         self.year = year
         self.cost = cost
+
+    def __str__(self):
+        """Return a nicely formatted guitar string."""
+        return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
+
