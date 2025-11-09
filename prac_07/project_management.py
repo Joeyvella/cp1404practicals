3fdee8610b90a5578c42404fbb8ba1ab3dedc79c
Joeyvella <joey.vella@my.jcu.edu.au>
2025-11-09 11:09:52 +1000

    Import project

---
+++ prac_07/project_management.py
@@ -0,0 +1,9 @@
+"""CP1404/CP5632 Practical - Project Management
+Estimate: 90 min | Actual: ___ min
+"""
+
+from datetime import datetime
+from project import Project
+
+DATE_FMT = "%d/%m/%Y"
+DEFAULT_FILENAME = "projects.txt"
