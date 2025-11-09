c3e3694088784d74fb07615fba6f2889fcc781b6
Joeyvella <joey.vella@my.jcu.edu.au>
2025-11-09 10:40:43 +1000

    Read the lines in the .csv file and part them out

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -8,3 +8,12 @@ def main():
 
     in_file = open('guitars.csv', 'r')
     in_file.readline()
+
+    for line in in_file:
+        # print(repr(line))  # debugging
+        parts = line.strip().split(',')
+        # print(parts)       # debugging
+
+        name = parts[0]
+        year = int(parts[1])
+        cost = float(parts[2])
