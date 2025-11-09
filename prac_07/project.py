8901bf203e8255fcad931fe55cb547545ab564d9
Joeyvella <joey.vella@my.jcu.edu.au>
2025-11-09 11:12:00 +1000

    Start a class and define wats in it

--- prac_07/project.py
+++ prac_07/project.py
@@ -5,3 +5,13 @@ Estimate: 45 min | Actual: ___ min
 from datetime import datetime
 
 DATE_FMT = "%d/%m/%Y"
+class Project:
+    """Represent a Project object."""
+
+    def __init__(self, name, start_date, priority, cost_estimate, completion):
+        """Initialise a Project instance."""
+        self.name = name
+        self.start_date = start_date
+        self.priority = priority
+        self.cost_estimate = cost_estimate
+        self.completion = completion
