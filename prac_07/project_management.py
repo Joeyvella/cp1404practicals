724bcc2d71183f5fdd748937836b4a83ac2088ce
Joeyvella <joey.vella@my.jcu.edu.au>
2025-11-09 12:37:09 +1000

    Display a menu for the user to choose inputs from

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -14,3 +14,13 @@ def main():
     print("Welcome to Pythonic Project Management")
     projects = load_projects(FILENAME)
     print(f"Loaded {len(projects)} projects from {FILENAME}")
+
+    MENU = (
+        "- (L)oad projects  \n"
+        "- (S)ave projects  \n"
+        "- (D)isplay projects  \n"
+        "- (F)ilter projects by date\n"
+        "- (A)dd new project  \n"
+        "- (U)pdate project\n"
+        "- (Q)uit"
+    )
