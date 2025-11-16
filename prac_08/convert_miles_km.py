4e3948f594090cf0cee1688a1b45e559c08d6eae
Joeyvella <joey.vella@my.jcu.edu.au>
2025-11-16 16:04:49 +1000

    Make a handle for converting the values

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -13,3 +13,16 @@ class MilesToKilometresApp(App):
     """App for converting miles to kilometres."""
     # This is bound to the output Label's text in the kv file
     output_text = StringProperty("0.0")
+
+    def build(self):
+        """Build the Kivy app from the kv file."""
+        self.title = "Convert Miles to Kilometres"
+        self.root = Builder.load_file("convert_miles_km.kv")
+        return self.root
+
+    def handle_convert(self):
+        """Convert the value in the TextInput from miles to km."""
+        miles = self.get_valid_miles()
+        kilometres = miles * MILES_TO_KM
+        # Update the StringProperty, which updates the label automatically
+        self.output_text = f"{kilometres:.5f}"
