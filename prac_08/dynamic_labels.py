2734fb88ec4d8eef7588be77b417412fe08293d4
Joeyvella <joey.vella@my.jcu.edu.au>
2025-11-16 16:47:15 +1000

    Build Kivy interface for the Labels

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -9,3 +9,20 @@ class DynamicLabelsApp(App):
         super().__init__()
         # This is the data (model)
         self.names = ["Alex", "Bob", "Ryan", "Dora", "Evan"]
+
+    def build(self):
+        """Build the Kivy interface and add labels."""
+        self.root = Builder.load_file("dynamic_labels.kv")
+
+        main_box = self.root.ids.main  # Get the BoxLayout with id 'main'
+
+        # Create a Label for each name and add it to the layout
+        from kivy.uix.label import Label
+        for name in self.names:
+            temp_label = Label(text=name)
+            main_box.add_widget(temp_label)
+
+        return self.root
+
+
+DynamicLabelsApp().run()
