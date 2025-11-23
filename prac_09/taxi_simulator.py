68c09c775abbeddcc68ef9038b0737795072223c
Joeyvella <joey.vella@my.jcu.edu.au>
2025-11-23 20:22:49 +1000

    Create a menu for the user to choose from

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -10,3 +10,26 @@ def main():
         SilverServiceTaxi("Limo", 100, 2),
         SilverServiceTaxi("Hummer", 200, 4)
     ]
+    current_taxi = None
+    bill = 0
+
+    menu = "q)uit, c)hoose taxi, d)rive\n>>> "
+
+    user_choice = input(menu).lower()
+    while user_choice != "q":
+        if user_choice == "c":
+            current_taxi = choose_taxi(taxis)
+        elif user_choice == "d":
+            if current_taxi is None:
+                print("You need to choose a taxi before you can drive")
+            else:
+                bill += drive_taxi(current_taxi)
+        else:
+            print("Invalid option")
+
+        print(f"Bill to date: ${bill:.2f}")
+        user_choice = input(menu).lower()
+
+    print(f"Total trip cost: ${bill:.2f}")
+    print("Taxis are now:")
+    display_taxis(taxis)
