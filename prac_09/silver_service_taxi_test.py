417056785d644a9f802eae97f3062a2b25121ac7
Joeyvella <joey.vella@my.jcu.edu.au>
2025-11-23 20:07:37 +1000

    Test fanciness multiplier and flagfall

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -1,11 +1,8 @@
-from silver_service_taxi import SilverServiceTaxi
+from prac_09.silver_service_taxi import SilverServiceTaxi
 
-# Create taxi: name, fuel, fanciness
-taxi = SilverServiceTaxi("Hummer", 200, 4)
 
-# Drive 18 km
-taxi.start_fare()
-taxi.drive(18)
+def main():
+    # Test fanciness multiplier + flagfall
+    hummer = SilverServiceTaxi("Hummer", 200, 2)
+    hummer.drive(18)
 
-# Calculate fare
-fare = taxi.get_fare()
