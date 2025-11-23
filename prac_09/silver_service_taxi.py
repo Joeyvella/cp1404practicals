9712084ed246e9b41f353d731949b68c4371123b
Joeyvella <joey.vella@my.jcu.edu.au>
2025-11-23 15:23:45 +1000

    Return fare based on variables and add flagfall at the end of it

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -15,3 +15,11 @@ class SilverServiceTaxi(Taxi):
         self.fanciness = fanciness
         # Scale the price per km (instance variable)
         self.price_per_km = Taxi.price_per_km * fanciness
+
+    def get_fare(self):
+        """Return fare: base fare * distance + flagfall."""
+        return super().get_fare() + self.flagfall
+
+    def __str__(self):
+        """Add flagfall info at the end."""
+        return f"{super().__str__()} plus flagfall of ${self.flagfall:.2f}"
