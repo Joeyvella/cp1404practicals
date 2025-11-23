c4897f01ba81ce5e919e29209d422188000b07e9
Joeyvella <joey.vella@my.jcu.edu.au>
2025-11-23 15:22:59 +1000

    Create a specialised taxi service class

---
+++ prac_09/silver_service_taxi.py
@@ -0,0 +1,17 @@
+"""
+SilverServiceTaxi class
+"""
+
+from taxi import Taxi
+
+
+class SilverServiceTaxi(Taxi):
+    """Specialised Taxi that includes fanciness and flagfall."""
+
+    flagfall = 4.50
+
+    def __init__(self, name, fuel, fanciness):
+        super().__init__(name, fuel)
+        self.fanciness = fanciness
+        # Scale the price per km (instance variable)
+        self.price_per_km = Taxi.price_per_km * fanciness
