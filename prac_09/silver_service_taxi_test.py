ec80b61a7853c0f9283371a4811661bf6b09a585
Joeyvella <joey.vella@my.jcu.edu.au>
2025-11-23 15:27:27 +1000

    Drive the taxi a certain distance and calculate the fare

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -2,3 +2,10 @@ from silver_service_taxi import SilverServiceTaxi
 
 # Create taxi: name, fuel, fanciness
 taxi = SilverServiceTaxi("Hummer", 200, 4)
+
+# Drive 18 km
+taxi.start_fare()
+taxi.drive(18)
+
+# Calculate fare
+fare = taxi.get_fare()
