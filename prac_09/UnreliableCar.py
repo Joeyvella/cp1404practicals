e8c7ef98a1abb704622fdc4b7a445473473d49c1
Joeyvella <joey.vella@my.jcu.edu.au>
2025-11-23 14:59:35 +1000

    Decide if the car is reliable or not, if it is it will return a distance of how far it drove

--- prac_09/UnreliableCar.py
+++ prac_09/UnreliableCar.py
@@ -14,3 +14,10 @@ class UnreliableCar(Car):
         """Initialise an UnreliableCar, based on parent class Car."""
         super().__init__(name, fuel)
         self.reliability = float(reliability)
+
+    def drive(self, distance):
+        """Drive car a random distance between 0 and 100, choose if it will be reliable then return distance driven."""
+        chance = random.uniform(0,100)
+        if chance < self.reliability:
+            return super().drive(distance)
+        return 0
